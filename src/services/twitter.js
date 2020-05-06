a6be2ba256cf504be479230c6c392e8ff03727fa
LaksCastro <lakhsmycastro336@gmail.com>
2020-05-06 01:52:40 -0300

    remove unnecessary function call

--- src/services/twitter.js
+++ src/services/twitter.js
@@ -1,7 +1,7 @@
 const { get } = require("../client");
 
 const TwitterApiFactory = () => {
-  const requestReply = (imageData, getStatus, onComplete = () => {}) => {
+  const requestReply = (imageData, getStatus) => {
     let client = get();
 
     client.post("media/upload", { media: imageData }, function (error, media) {
@@ -12,8 +12,6 @@ const TwitterApiFactory = () => {
 
         client.post("statuses/update", status, function (error) {
           if (error) return console.log(error);
-
-          onComplete();
         });
       }
     });
